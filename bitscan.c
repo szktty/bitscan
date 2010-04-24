1d478e6f6ba6c9ee848844c87f31fef0673974cf
SUZUKI Tetsuya <suzuki@spice-of-life.net>
2010-04-25 02:32:12 +0900

    rough implementation

--- bitscan.c
+++ bitscan.c
@@ -22,3 +22,70 @@
 
 #include "bitscan.h"
 
+static bitalloc default_alloc = {
+  malloc,
+  realloc,
+  free
+};
+
+#define GROWRATE        1.6
+#define GROWSIZE(n)     (size_t)((n)*GROWRATE)
+
+#define AT(bytes,idx)     ((uint8_t *)(bytes))[(idx)/8]
+#define GET(bytes,idx)    (AT(bytes,idx) & (1<<(8-(idx)%8)))
+#define SET(bytes,idx,v)  \
+  AT(bytes,idx) = (AT(bytes,idx) & ~(1<<(8-(idx)%8))) | (v)
+
+bitarray *
+bitmake(void *buf, size_t pos, size_t size, const bitalloc *alloc)
+{
+  bitarray *bits;
+  size_t bpos = 0;
+
+  if (alloc == NULL)
+    alloc = &default_alloc;
+
+  bits = (bitarray *)alloc->alloc(sizeof(bitarray));
+  bits->_alloc = alloc;
+  bits->_capa = GROWSIZE((size / 8) + 1);
+  bits->_bytes = (uint8_t *)alloc->alloc(bits->_capa);
+  bits->_pos = 0;
+  bits->_bitsize = size;
+
+  if (pos == 0) {
+    memmove(bits->_bytes, buf, pos / 8);
+    bpos = pos / 8 * 8;
+  }
+
+  for (; bpos < size; pos++, bpos++) {
+    SET(bits->_bytes, bpos, GET(buf, pos));
+  }
+
+  return bits;
+}
+
+void
+bitfree(bitarray *bits)
+{
+  bits->_alloc->free(bits->_bytes);
+  bits->_alloc->free(bits);
+}
+
+size_t
+bitsize(const bitarray *bits)
+{
+  return bits->_bitsize;
+}
+
+uint8_t
+bitget(const bitarray *bits, size_t index)
+{
+  return GET(bits->_bytes, index);
+}
+
+void
+bitset(bitarray *bits, size_t index, uint8_t value)
+{
+  SET(bits->_bytes, index, value);
+}
+
