f7337758ee21f01174d0d60ebe2b09f5074d39b2
SUZUKI Tetsuya <suzuki@spice-of-life.net>
2010-04-27 03:26:48 +0900

    fixed

--- test/testmake.c
+++ test/testmake.c
@@ -16,9 +16,9 @@ static void **
 datatestmake_copy()
 {
   static struct datatestmake **data = NULL;
-  static size_t n = 100, i, j;
+  static size_t n = 100, i, j, last, bytesize;
   struct datatestmake *test;
-  uint8_t v;
+  uint8_t v, shift;
 
   if (data == NULL) {
     data = (struct datatestmake **)
@@ -26,14 +26,19 @@ datatestmake_copy()
 
     for (i = 0; i < n; i++) {
       test = (struct datatestmake *)malloc(sizeof(struct datatestmake));
-      test->pos = 0;
+      shift = (uint8_t)(random() % 8);
+      test->pos = shift;
       test->size = (size_t)(random() % 2049);
-      test->bytes = malloc((test->size / 8) + 1);
-      test->expected = malloc((test->size / 8) + 1);
-      for (j = 0; j < test->size / 8; j++) {
+      bytesize = (test->size / 8) + 2;
+      test->bytes = malloc(bytesize);
+      test->expected = malloc(bytesize);
+      memset(test->expected, 0, bytesize);
+      last = (test->size / 8) + (test->size % 8 > 0);
+      for (j = 0; j < last; j++) {
         v = (size_t)(random() % 256);
         test->bytes[j] = v;
-        test->expected[j] = v;
+        test->expected[j] |= v >> shift;
+        test->expected[j+1] = v << (8 - shift);
       }
       data[i] = test;
     }
@@ -51,7 +56,7 @@ testmake_copy(void *data)
 
   test = (struct datatestmake *)data;
   bits = bitmake(test->bytes, test->pos, test->size, true, NULL);
-  testassert(test->pos == bits->_pos, "wrong pos");
+  testassert(0 == bits->_pos, "wrong pos");
   testassert(test->size == bits->_size, "wrong size");
   testassert(rawbiteq(test->expected, test->pos,
         bits->_bytes, bits->_pos, bits->_size),
